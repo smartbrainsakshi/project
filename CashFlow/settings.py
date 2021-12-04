5c9dda266e151c872c7773377fc8eee53acc94c9
codesOfSakshi <sakshi.mishra@sjsu.edu>
2021-12-04 09:00:36 -0800

    "host changes"

--- CashFlow/settings.py
+++ CashFlow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0_uoar=w=91k-t8omq7+_u@^b2=5(11m*d8bo4@igmg@da7%h8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
